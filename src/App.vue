e36964cd7b748be3fd077931469b6a7a8d1437dd
Alex Khazhakyan <askhazhakyan@cpp.edu>
2022-04-08 21:20:45 -0700

    Added fundamental game functions (calculateWinner function taken from React.js Tutorial site)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,67 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import {ref,computed} from 'vue'
+
+  const player = ref("X");
+  const gameBoard = ref([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ])
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      //calculating if theres 3 of the same symbol in the same row
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //calculating if theres 3 of the same symbol in the same columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      //calculating if theres 3 of the same symbol in the diagonals
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = computed(() => calculateWinner(gameBoard.value.flat()))
+
+  const playerMove = (x,y) => {
+    if (winner.value){
+      return
+    }
+
+    if (gameBoard.value[x][y] !== ""){
+      return
+    }
+
+    gameBoard.value[x][y] == player.value
+
+    player.value = player.value === 'X' ? 'O' : 'X'
+  }
+
+  const boardClear = () => {
+    gameBoard.value = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ]
+
+    player.value = 'X'
+  }
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Hello, World</h1>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
